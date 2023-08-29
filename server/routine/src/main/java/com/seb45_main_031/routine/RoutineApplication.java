3593e65ed2ec90cd2771d1b31976c513dfc33880
0KSY <uryhon7@gmail.com>
2023-08-29 17:10:08 +0900

    :tada:Document:BE 개발 환경 세팅 프로젝트 생성 기본 디렉토리 구조 설정 의존성 추가 Issues:#13

---
+++ server/routine/src/main/java/com/seb45_main_031/routine/RoutineApplication.java
@@ -0,0 +1,13 @@
+package com.seb45_main_031.routine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutineApplication.class, args);
+	}
+
+}
---
+++ server/routine/src/test/java/com/seb45_main_031/routine/RoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb45_main_031.routine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
