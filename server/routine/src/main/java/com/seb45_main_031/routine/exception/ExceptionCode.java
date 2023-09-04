de03a3a0fb89b922174f134a44abe0f5ced56a97
0KSY <uryhon7@gmail.com>
2023-09-04 14:39:24 +0900

    Feat : Tag CRUD 기능 추가 Controller 핸들러 메소드 추가 Service 비지니스 로직 추가 Repository 메소드 추가 Dto 추가 Mapper 추가 + Member Service -> checkId -> public 수정 + ExceptionCode 추가 Issues : #18

--- server/routine/src/main/java/com/seb45_main_031/routine/exception/ExceptionCode.java
+++ server/routine/src/main/java/com/seb45_main_031/routine/exception/ExceptionCode.java
@@ -9,7 +9,8 @@ public enum ExceptionCode {
     MEMBER_EXISTS(409, "Member exists"),
     FEED_NOT_FOUND(404, "Feed not found"),
     COMMENT_NOT_FOUND(404, "Comment not found"),
-    TODO_NOT_FOUND(404, "Todo not found");
+    TODO_NOT_FOUND(404, "Todo not found"),
+    TAG_NOT_FOUND(404, "Tag not found");
 
 
     @Getter
--- server/routine/src/main/java/com/seb45_main_031/routine/tag/mapper/TagMapper.java
+++ server/routine/src/main/java/com/seb45_main_031/routine/tag/mapper/TagMapper.java
@@ -4,9 +4,15 @@ import com.seb45_main_031.routine.tag.dto.TagDto;
 import com.seb45_main_031.routine.tag.entity.Tag;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TagMapper {
 
     Tag tagPostDtoToTag(TagDto.Post tagPostDto);
+
+    TagDto.Response tagToTagResponseDto(Tag tag);
+
+    List<TagDto.Response> tagsToTagResponseDtos(List<Tag> tags);
 
 }
