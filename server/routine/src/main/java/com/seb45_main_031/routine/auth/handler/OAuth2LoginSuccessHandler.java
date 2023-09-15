f65a45761f682298cbd731738479212ef2106bb4
0KSY <uryhon7@gmail.com>
2023-09-15 20:12:03 +0900

    Feat : OAuth2 구현 Google OAuth2 로그인 구현 Member SignupType 컬럼 추가 Issues : #151

---
+++ server/routine/src/main/java/com/seb45_main_031/routine/auth/handler/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,149 @@
+package com.seb45_main_031.routine.auth.handler;
+
+import com.seb45_main_031.routine.auth.jwt.JwtTokenizer;
+import com.seb45_main_031.routine.auth.utils.CustomAuthorityUtils;
+import com.seb45_main_031.routine.member.entity.Member;
+import com.seb45_main_031.routine.member.repository.MemberRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.util.*;
+
+public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils customAuthorityUtils;
+    private final MemberRepository memberRepository;
+
+    public OAuth2LoginSuccessHandler(JwtTokenizer jwtTokenizer,
+                                     CustomAuthorityUtils customAuthorityUtils,
+                                     MemberRepository memberRepository) {
+        this.jwtTokenizer = jwtTokenizer;
+        this.customAuthorityUtils = customAuthorityUtils;
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        var oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String email = String.valueOf(oAuth2User.getAttributes().get("email"));
+        String nickname = String.valueOf(oAuth2User.getAttributes().get("name"));
+
+        Optional<Member> findMemberByEmailAndSignupType
+                = memberRepository.findByEmailAndSignupType(email, Member.SignupType.GOOGLE_OAUTH2);
+
+        if(findMemberByEmailAndSignupType.isPresent()){
+
+            redirect(request, response, findMemberByEmailAndSignupType.get());
+
+        }
+        else{
+
+            Member savedMember = saveMemberToDB(email, nickname);
+            redirect(request, response, savedMember);
+
+        }
+
+    }
+
+    private Member saveMemberToDB(String email, String nickname){
+        Member member = new Member();
+        member.setEmail(email);
+
+        member.setSignupType(Member.SignupType.GOOGLE_OAUTH2);
+
+        List<String> roles = customAuthorityUtils.createRoles(email);
+
+        member.setRoles(roles);
+
+        Optional<Member> findMemberByNickname = memberRepository.findByNickname(nickname);
+
+        if(findMemberByNickname.isPresent()){
+            nickname = nickname + System.currentTimeMillis();
+            member.setNickname(nickname);
+        }
+        else{
+            member.setNickname(nickname);
+        }
+
+        return memberRepository.save(member);
+
+    }
+
+    private void redirect(HttpServletRequest request, HttpServletResponse response, Member member) throws IOException{
+
+        String accessToken = "Bearer " + delegateAccessToken(member);
+        String refreshToken = delegateRefreshToken(member);
+
+        String uri = createURI(accessToken, refreshToken, member).toString();
+        getRedirectStrategy().sendRedirect(request, response, uri);
+    }
+
+    private String delegateAccessToken(Member member){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("memberId", member.getMemberId());
+        claims.put("username", member.getEmail());
+        claims.put("roles", member.getRoles());
+
+        String subject = member.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+
+    private String delegateRefreshToken(Member member){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("memberId", member.getMemberId());
+
+        String subject = member.getEmail();
+
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String refreshToken = jwtTokenizer.generateRefreshToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
+
+    private URI createURI(String accessToken, String refreshToken, Member member){
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+        queryParams.add("access_token", accessToken);
+        queryParams.add("refresh_token", refreshToken);
+        queryParams.add("memberId", String.valueOf(member.getMemberId()));
+        queryParams.add("email", member.getEmail());
+        queryParams.add("nickname", member.getNickname());
+        queryParams.add("exp", String.valueOf(member.getExp()));
+        queryParams.add("level", String.valueOf(member.getLevel()));
+//        queryParams.add("image", member.getImage());
+        if(member.getImage() != null){
+            queryParams.add("image", member.getImage());
+        }
+
+        return UriComponentsBuilder
+                .newInstance()
+                .scheme("http")
+                .host("seb45main31.s3-website.ap-northeast-2.amazonaws.com") // s3 url
+//                .port(80)
+                .path("/receive-token.html") // 쿼리 파라미터 전달되는 정보 받을 프론트 측 s3 url path
+                .queryParams(queryParams)
+                .build()
+                .toUri();
+    }
+}
